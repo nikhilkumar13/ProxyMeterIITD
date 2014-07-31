73a995eba27224f7f7a385d1cf5178adda356c0a
Nikhil Kumar <nikkr007@gmail.com>
2014-07-31 11:09:42 +0530

    proxymeter about to pushed

---
+++ README.md
@@ -0,0 +1,6 @@
+This is small python code that can show you your IIT-D proxy usage in ubuntu app indicator top bar.
+Open the proxycheck.py file  set your username password , path to icon (png) image , frequecny of your choice(default is 15 mint)
+and run the Code. After that you may be have to install a dependency, python app indicator package.
+Then your proxy meter is all set to check your proxy usage after every 15 minute and display the usage in top bar
+Tested on ubuntu 14.04!
+Also you can add this code to startup application to run the proxyMeter automaticaly when system starts
---
+++ proxycheck.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import appindicator
+import pynotify
+import gtk
+import os
+
+username="cs1120238"
+password="nikhil00-"
+frequency=15*60*1000  #15 minutes
+iconPath='/home/nikhil/Desktop/icon.png'   #set path to your own icon
+
+
+a = appindicator.Indicator('tubecheck', iconPath, appindicator.CATEGORY_APPLICATION_STATUS)
+a.set_status(appindicator.STATUS_ACTIVE)
+m = gtk.Menu() 
+qi = gtk.MenuItem( 'Quit' )
+ch=gtk.MenuItem('Check')
+m.append(ch)
+m.append(qi)
+
+a.set_menu(m)
+ch.show()
+qi.show()
+def getData(username,password):
+	command= 'curl -k -d'+ '"uid='+username+'&magic_word='+password+'&1=Proxy Usage"'+' https://proxy22.iitd.ernet.in/squish/'+ ' | sed -n "35,35p;35q"'+'| grep -o "[1-9]\S*[Gb|mb]"'
+	p=os.popen(command,"r")
+	data=p.readline()
+	return  data.strip()
+def Check(item):
+	data=getData(username,password)
+	a.set_label("(" +data +")")
+	return True
+def update():
+	data=getData(username,password)
+	a.set_label("(" +data +")")
+	return True
+def quit(item):
+	gtk.main_quit()
+qi.connect('activate', quit)
+ch.connect('activate',Check)
+data=getData(username,password)
+a.set_label("(" +data +")")
+gtk.timeout_add(frequency, update)
+gtk.main()
+
+
